d503761ee73bc0102933c33da8ae9b41ef9da68c
Kartik <kartik.kalra@ireslab.com>
2019-02-26 11:17:10 +0530

    Test

---
+++ src/test/java/Login/loginFunctionality.java
@@ -0,0 +1,26 @@
+package Login;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class loginFunctionality 
+{
+
+	@Test
+	public void Login_Scenarioo()
+	{
+		 System.setProperty("webdriver.gecko.driver", "C:\\Users\\Abhinav.IRESLAB\\Desktop\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com");
+		
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.findElement(By.id("txtUsername")).sendKeys("admin");
+		driver.findElement(By.name("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.name("Submit")).click();
+	}
+
+}
